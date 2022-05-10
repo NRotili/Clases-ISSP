7e552e6e8f45f65e945a465d2938931f293d69a4
NRotili <rotilinicolas@gmail.com>
2022-05-09 21:54:12 -0300

    Act. 2022-5-9

--- 8. Funciones/1.funciones.php
+++ 8. Funciones/1.funciones.php
@@ -1,10 +1,22 @@
 <?php
 
 declare(strict_types=1);
-function bienvenida($usuario = 'Invitado'){
+function bienvenida(string $usuario = 'Invitado'):string{
     return "Bienvenido $usuario";
 }
-echo bienvenida();
+echo bienvenida('Nicolás');
+
+
+function sumar2(int ...$numeros):int{
+    return array_sum($numeros);
+}
+echo sumar(1,2,3,4,5);
+
+
+function sumar($numero1, $numero2):int{
+    return $numero1+$numero2;
+}
+echo sumar(1,2,3,4,5);
 
 // date_default_timezone_set('America/Argentina/Buenos_Aires');
 // $date = new DateTime();
