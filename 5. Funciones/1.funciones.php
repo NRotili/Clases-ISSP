1e65c8cab24777cfb3d9c2aa73ba0dc5fc6118ea
NRotili <rotilinicolas@gmail.com>
2022-05-16 07:13:59 -0300

    Act. 2022-5-16

--- 5. Funciones/1.funciones.php
+++ 5. Funciones/1.funciones.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 // function bienvenida(string $usuario = 'Invitado'):string{
@@ -12,10 +13,11 @@ declare(strict_types=1);
 // bienvenida();
 
 
-function sumar(int ...$numeros):int{
-    return array_sum($numeros);
-}
-echo sumar(1,2,3,4,5,6,7);
+// function sumar(int ...$numeros): int
+// {
+//     return array_sum($numeros);
+// }
+// echo sumar(1, 2, 3, 4, 5, 6, 7);
 
 // function sumar(int $numero1, int $numero2): int
 // {
@@ -31,3 +33,36 @@ echo sumar(1,2,3,4,5,6,7);
 
 
 // echo date('d-m-Y');
+
+//Calcular factorial
+function factorial(int $numero): int
+{
+    if ($numero == 0) {
+        return 1;
+    }
+    return $numero * factorial($numero - 1);
+}
+// echo factorial(6);
+
+//Calcular potencia
+function potencia(int $base, int $exponente): int
+{
+    
+    return $base**$exponente;
+}
+// echo potencia(6, 3);
+
+//Mayor de un array
+function mayor(array $numeros): float
+{
+    $mayor = $numeros[0];
+    foreach ($numeros as $numero) {
+        if ($numero > $mayor) {
+            $mayor = $numero;
+        }
+    }
+    return $mayor;
+}
+
+// echo mayor([1, 2, 3, 4, 5, 6, 7, 7.5]);
+
