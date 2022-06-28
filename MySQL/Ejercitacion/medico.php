b90a2ac3033624bfb8bdb6b625d59049ddfe88c8
NRotili <rotilinicolas@gmail.com>
2022-06-28 13:55:57 -0300

    Act. 2022-06-28

--- MySQL/Ejercitacion/medico.php
+++ MySQL/Ejercitacion/medico.php
@@ -1,5 +1,17 @@
 <?php
 require_once './Clases/Medico.php';
-$medico = Medico::getMedicoById(1);
+require_once './Clases/Especialidad.php';
+// $medico = Medico::getMedicoById(1);
 
-echo $medico->especialidad()->especialidad;
+// echo $medico->especialidad()->especialidad;
+
+//Solicitamos la especialidad Pediatría
+$especialidad = Especialidad::getEspecialidadById(4);
+//Buscamos los médicos con especialidad Pediatría
+$medicos = $especialidad->medicos();
+//Recorremos con foreach los pediatras.
+foreach ($medicos as $medico) {
+    echo "$medico->nombre $medico->apellido <br>";
+}
+
+
