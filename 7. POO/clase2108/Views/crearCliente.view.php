57701677c1b3fff4a86b69615614300da226e1d7
ROTNIC <rotilinicolas@gmail.com>
2025-08-28 19:47:57 -0300

    Clase 2808

---
+++ 7. POO/clase2108/Views/crearCliente.view.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <div class="row">
+            <label for="nombre" class="form-label">Nombre</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" required>
+
+        </div>
+        <div class="row">
+
+            <label for="apellido" class="form-label">Apellido</label>
+            <input type="text" class="form-control" id="apellido" name="apellido" required>
+
+        </div>
+
+        <div class="row">
+
+            <label for="fecnac" class="form-label">Fecha Nacimiento</label>
+            <input type="text" class="form-control" id="fecnac" name="fecnac" required>
+
+        </div>
+        <div class="row">
+
+            <label for="email" class="form-label">Email</label>
+            <input type="text" class="form-control" id="email" name="email" required>
+
+        </div>
+        <div class="row">
+
+            <label for="edad" class="form-label">Edad</label>
+            <input type="text" class="form-control" id="edad" name="edad" required>
+
+        </div>
+        <div class="row">
+
+            <label for="id_ciudad" class="form-label">Ciudad</label>
+            <input type="text" class="form-control" id="id_ciudad" name="id_ciudad" required>
+
+        </div>
+
+    </form>
+
+</body>
+
+</html>
